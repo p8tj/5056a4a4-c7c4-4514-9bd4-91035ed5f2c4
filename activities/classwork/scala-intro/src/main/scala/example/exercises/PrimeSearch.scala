f01e65921d031e150a800e794b7b9f6476b3d238
p8tj <ivan.garcia04097@gmail.com>
2019-11-26 23:40:28 -0600

    cambios

--- activities/classwork/scala-intro/src/main/scala/example/exercises/PrimeSearch.scala
+++ activities/classwork/scala-intro/src/main/scala/example/exercises/PrimeSearch.scala
@@ -1,24 +1,25 @@
 package example.exercises
 
-object PrimeSearch {
- /**
-  * Problem definition: Find all the prime numbers in between two natural numbers.
-  *
-  * Prime number is defined as a natural number grater than one that cannot be composed by with the multiplication
-  * of two smaller natural numbers.
-  *
-  * Consider:
-  *    - Using the option-type safely transform the input values.
-  *    - Create your own tests for the isPrime function.
-  *    - Return a string separated by commas: 2, 3, 5, 7, 11, 13
-  */
+object ReversePrimeSearch {
+  /**
+   * Problem definition: Find all the reverse-prime numbers in between two natural numbers.
+   *
+   * A reverse-prime number is defined as a prime number that when "read backwards" is also a prime number.
+   * Example: 17 is a reversible prime number.
+   *
+   * Consider:
+   *    - Using the option-type safely transform the input values.
+   *    - Use scala implicits to transform from String => Option[Long]
+   *    - Create your own tests for the isReversePrime function.
+   *    - Return a string separated by commas: 13, 17, 31, 37
+   */
 
-  def isPrime(num: Long): Boolean = ???
+  def isReversePrime(num: Long): Boolean = ???
 
   def main(args: Array[String]): Unit = {
     val Array(start, end) = args
 
-    println(start, end)
+    println(start)
   }
 
 }
