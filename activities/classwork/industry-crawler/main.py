2112071058d6a063643189390d86913966bec584
p8tj <ivan.garcia04097@gmail.com>
2019-12-02 21:49:07 -0600

    cambios

--- activities/classwork/industry-crawler/main.py
+++ activities/classwork/industry-crawler/main.py
@@ -9,21 +9,44 @@ from util import StringWrapper, pretty_print, timeit
 URL = "https://www.osha.gov/pls/imis/sic_manual.html"
 DEFAULT_INDUSTRY_FILE = "industries.json"
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(_name_)
 
 
 class Main(object):
 
+    def _recursive_search(self, node, string_wrapper, exact):
+        title = node["title"]
+        children = node["children"]
+        new_children = []
+        for child in children:
+            child_valid = self._recursive_search(child, string_wrapper, exact=exact)
+            if child_valid:
+                new_children.append(child)
+        node["children"] = new_children
+        result = len(new_children) or string_wrapper.boolean_search(title, reverse=True, exact=exact)
+        return result
+
     @staticmethod
     @timeit(logger)
     def download(filename=DEFAULT_INDUSTRY_FILE):
-        pass
+        logger.info("Starting download procedure...")
+        sic = SIC.from_url(URL)
+        with open(filename, "w") as file:
+            file.write(sic.jsonify())
 
     @timeit(logger)
+    @pretty_print(logger)
     def search(self, title, exact=False, filename=DEFAULT_INDUSTRY_FILE):
-        pass
+        target_title = StringWrapper(value=title)
+        sic_industries = SIC.load_json(filename)
+        children = sic_industries["children"]
+        new_children = []
+        for child in children:
+            if self._recursive_search(child, target_title, exact=exact):
+                new_children.append(child)
+        return new_children
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     logging.basicConfig(level=logging.INFO)
     fire.Fire(Main)
